039a9c8ef70eb490ea83c32cee25a78e28ad670d
medStudent05 <78272493+medStudent05@users.noreply.github.com>
2021-03-06 20:48:16 +0530

    Initial commit

---
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.todo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        VideoView videoView = findViewById(R.id.videoview);
+        String videoPath = "https://www.dropbox.com/s/lgciqib0tdgef63/sample-mp4-file.mp4?dl=0";
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
+
+    }
+
+
+
+
+    }
+
+
+
+
+
+
+
+
